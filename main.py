64be7557e1c8ee9467c657500c9492f5c590552c
Kirill <kmonyov@mail.ru>
2023-10-23 23:56:20 +0300

    Added drawing sprites to main.py with some optimization

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from settings import *
 from player import Player
-from map import world_map
+from sprite_objects import *
 from ray_casting import rayCasting
 import math
 from drawing import Drawing
@@ -12,6 +12,7 @@ pygame.init()
 sc = pygame.display.set_mode((width, height))
 sc_map = pygame.Surface((width // map_scale, height // map_scale))
 
+sprites = Sprites()
 frames = pygame.time.Clock()
 player = Player()
 drawing = Drawing(sc, sc_map)
@@ -26,8 +27,8 @@ while True:
 
     # sky and floor
     drawing.background(player.angle)
-    drawing.world(player.pos, player.angle)
-
+    walls = rayCasting(player, drawing.textures)
+    drawing.world(walls + [obj.object_locate(player, walls) for obj in sprites.list_of_objects])
     drawing.fps(frames)
 
     drawing.mini_map(player)
